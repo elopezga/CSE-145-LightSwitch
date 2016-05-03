9a199ae7d64180f53881a89d4912f08eae8cacea
Edgar Lopez <edgarylop@gmail.com>
2016-05-02 18:40:01 -0700

    Arduino code from desktop

--- Arduino/sketch_apr26a/sketch_apr26a.ino
+++ Arduino/sketch_apr26a/sketch_apr26a.ino
@@ -11,18 +11,22 @@
 // give it a name:
 int led = 13;
 
-SoftwareSerial toESP2866(10, 11); // Specify RX and TX pins on Arduino (RX, RX) as serial
+SoftwareSerial toESP2866(11, 10); // Specify RX and TX pins on Arduino (TX, RX) as serial
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
-  monitor.begin(9600);
+ 
   Serial.begin(9600);  
+  toESP2866.begin(9600);
   
   Serial.println("Reseting ESP2866");
   delay(2000);
   toESP2866.println("AT+RST");
+  delay(200);
+  Serial.println("Connecting to network");
+  connectWifi("cozy", "juanisdumb");
 }
 
 // the loop routine runs over and over again forever:
@@ -39,9 +43,19 @@ void loop() {
   delay(2000);
   monitor.println("AT+RST");
   */
+  toESP2866.listen();
+  toESP2866.println("AT+CWMODE?");
+  delay(500);
+  //Serial.println(String(toESP2866.read()));
+  //Serial.println(readLine(toESP2866));
+  while( toESP2866.available() >0 ){
+    Serial.print("Response: ");
+    Serial.write(toESP2866.read());
+  }
+  Serial.println("Done");
 }
 
-void connectWifi(){
+void connectWifi(String Ssd, String Pass){
   Serial.println("Setting WiFi mode to BOTH (3)");
   String cmd = "AT+CWMODE:3";
   toESP2866.println(cmd);
@@ -54,9 +68,35 @@ void connectWifi(){
   Serial.println("Done");
 
   // Connect to wifi network
-  cmd = "AT+CWJAP=\"SSD\",\"PASS\"";  
-  
+  cmd = "AT+CWJAP=\""+Ssd+"\",\""+Pass+"\"";
+  Serial.println("Sending command: " + cmd);
+  toESP2866.println(cmd);
+  Serial.println("Done");
+  delay(1000);
+  //toESP2866.println("AT+CIFSR");
+  //delay(10000);
+  //Serial.println(readLine(toESP2866));
+  //Serial.println("Ready to start working!");  
 }
+
+String readLine(SoftwareSerial mon){
+  char c;
+  String msg;
+  String cstr;
+
+  //mon.listen();
+  /*
+  do{
+    c = mon.read();
+    cstr = String(c);
+    Serial.println(cstr);
+    msg = msg + cstr;
+    Serial.println(msg);
+  } while (cstr!="\n");*/
+
+  return msg;
+}
+
 
 // Send to android the GET data
 
